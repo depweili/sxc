279dd7720de4bde0f84becf4d8c4f665a3b73e47
depweili <weili_9135@hotmail.com>
2017-11-26 18:11:19 +0800

    佣金1.0

--- SXC.Core/Models/Agent.cs
+++ SXC.Core/Models/Agent.cs
@@ -1,4 +1,5 @@
 ﻿using SXC.Code;
+using SXC.Code.Utility;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -47,4 +48,87 @@ namespace SXC.Core.Models
 
         public virtual User User { get; set; }
     }
+
+    public class UserPayment
+    {
+        public UserPayment()
+        {
+            IsDistr = true;
+            CreateTime = DateTime.Now;
+            PayUID = Guid.NewGuid();
+            PaySN = Constant.SNPREFIX_PAYMENT + RandomHelper.GetTimeRandom1();
+        }
+
+        public int ID { get; set; }
+
+        public int UserID { get; set; }
+        public virtual User User { get; set; }
+
+        public int PayItemID { get; set; }
+        public virtual PaymentItem PayItem { get; set; }
+
+        public Guid PayUID { get; set; }
+
+        public string PaySN { get; set; }
+
+        public bool IsDistr { get; set; }
+
+        public int DistrType { get; set; }
+
+        public int State { get; set; }
+
+        public decimal Amount { get; set; }
+
+        public decimal DistrAmount { get; set; }
+
+        public decimal Commission { get; set; }
+
+        public decimal FinalAmount { get; set; }
+
+        public DateTime CreateTime { get; set; }
+
+        public DateTime? DistrTime { get; set; }
+
+        public DateTime? AccountTime { get; set; }
+
+        public string Memo { get; set; }
+
+        public string OperatorID { get; set; }
+
+        public string OperatorName { get; set; }
+
+        public virtual ICollection<CommissionRecord> CommissionRecords { get; set; }
+    }
+
+    public class CommissionRecord
+    {
+        public int ID { get; set; }
+
+        public decimal Commission { get; set; }
+
+        public int UserPaymentID{ get; set; }
+        public virtual UserPayment UserPayment { get; set; }
+
+        public int AgentID { get; set; }
+        public virtual Agent Agent { get; set; }
+
+        public int State { get; set; }
+
+        public DateTime CreateTime { get; set; }
+
+        public DateTime? CheckTime { get; set; }
+
+        public string Memo { get; set; }
+        
+    }
+
+    public class PaymentItem
+    {
+        public int ID { get; set; }
+
+        public string Name { get; set; }
+    }
+
+
+
 }
